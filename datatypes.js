2530f1c6109d8ddef7258fc6d0bbf47e64791d4e
paras <rawatparas@776gmail.com>
2025-09-23 22:10:19 +0530

    data types

---
+++ datatypes.js
@@ -0,0 +1,43 @@
+//    VAR data type
+
+var a= 34;
+if(true){
+    console.log(a)
+}
+
+// var is only accessible in the scope of the function
+
+function solve (){
+    var age = 5;
+    console.log(age)
+
+}
+
+// this is not allowed 
+// console.log(age)
+solve()
+
+
+// Same defination can create complexity in code 
+
+// var x =45;
+// var x =43; 
+
+// var data type can create debugging issues so we can use let data type
+// let data type 
+
+// let x =45;
+// let x =34;
+
+
+// not allowed 
+
+let x =34;  // this is a integer data type
+let y ="paras Rawat";  // this is a string data type
+console.log(x)
+console.log(y)
+
+
+
+
+
